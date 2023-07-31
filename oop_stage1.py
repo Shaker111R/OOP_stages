c5e9a205f707d61e67780cf7dce4ebbfc311566c
ViktorSh <shaker111@yandex.ru>
2023-07-31 23:27:18 +0300

    change file to stage 2

--- oop_stage1.py
+++ oop_stage1.py
@@ -8,7 +8,16 @@ class Student:
         self.grades = {}
  
     def add_courses(self, course_name):
-        self.finished_courses.append(course_name)   
+        self.finished_courses.append(course_name) 
+
+    def rate_lec(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.finished_courses and course in self.courses_in_progress:
+            if course in lecturer.lec:
+                lecturer.lec[course] += [grade]
+            else:
+                lecturer.lec[course] = [grade]
+        else:
+            return 'Ошибка'  
  
      
 class Mentor:
@@ -17,6 +26,10 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -25,22 +38,16 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-        
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
 
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviever = Reviewer('Some', 'Buddy')
+cool_reviever.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviever.rate_hw(best_student, 'Python', 10)
+cool_reviever.rate_hw(best_student, 'Python', 10)
+cool_reviever.rate_hw(best_student, 'Python', 10)
  
 print(best_student.grades)
